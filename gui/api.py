0aeaf52d8436f2247fa4e2e8381e4524382b7497
NiGo2000 <nick.goehlert@web.de>
2023-05-11 13:17:25 +0200

    commit

--- gui/api.py
+++
@@ -1,5 +0,0 @@
-# API from CoinGecko for Bitcoin price
-def bitcoinApi(cg):
-    data = cg.get_price(ids='bitcoin', vs_currencies='eur')
-    btc_price = float(data["bitcoin"]["eur"])
-    return btc_price
